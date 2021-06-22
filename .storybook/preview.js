f03267e351357200878f51912440009c85149576
Adrian Bielawski <adrian@bielaw.ski>
2021-06-22 16:59:22 +0100

    Initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import colorPalette from './colorPalette.json';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  colorPalette,
+}
