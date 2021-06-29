321a2ea065191f8ccc0746e63a07f009f2eb1672
Adrian Bielawski <adrian@bielaw.ski>
2021-06-29 10:59:53 +0100

    Removed shade name display if empty string

--- src/colorPicker/shadeTooltip.tsx
+++ src/colorPicker/shadeTooltip.tsx
@@ -8,29 +8,35 @@ type Props = {
     copied: boolean,
 };
 
-const ShadeTooltip = (props: Props) => (
-    <div
-        css={css`
+const ShadeTooltip = (props: Props) => {
+    const text = props.shade.label ?
+        `${props.shade.label}: ${props.shade.value}`
+        : props.shade.value;
+
+    return (
+        <div
+            css={css`
             padding: .2em .5em;
             border-radius: .3em;
             background-color: ${props.shade.value};
         `}
-    >
-        <p
-            css={css`
+        >
+            <p
+                css={css`
                 white-space: nowrap;
                 font-size: 1.2em;
                 line-height: 0;
                 color: ${props.shade.value};
                 filter: saturate(0) grayscale(1) brightness(1) contrast(100000%) invert(1);
             `}
-        >
-            {props.copied
-                ? 'Copied'
-                : `${props.shade.label}: ${props.shade.value}`
-            }
-        </p>
-    </div>
-);
+            >
+                {props.copied
+                    ? 'Copied'
+                    : text
+                }
+            </p>
+        </div>
+    )
+};
 
 export default ShadeTooltip;
