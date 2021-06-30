956921d52a78ed318760d2e4b44b1fa1377c7431
Adrian Bielawski <adrian@bielaw.ski>
2021-06-30 11:55:47 +0100

    Checkbox component created

---
+++ src/colorPicker/checkBox/checkBox.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { css, jsx } from '@emotion/react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+/** @jsx jsx */
+
+type Props = {
+    label?: string,
+    checked: boolean,
+    onClick: () => void,
+}
+
+const CheckBox = (props: Props) => (
+    <div
+        onClick={props.onClick}
+        css={css`
+            display: flex;
+            align-items: center;
+
+            &:hover {
+                cursor: pointer;
+            }
+        `}
+    >
+        <div
+            css={css`
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                border: 1px solid #777;
+                border-radius: .2em;
+                width: 1.3em;
+                height: 1.3em;
+                margin-right: .5em
+            `}
+        >
+            {props.checked && (
+                <FontAwesomeIcon icon={faCheck} color="#777" />
+            )}
+        </div>
+        {props.label && (
+            <p
+                css={css`
+                    font-size: 1.1em;
+                    line-height: 1em;
+                    margin: 0;
+                    color: #000;
+                    white-space: nowrap;
+                `}
+            >
+                {props.label}
+            </p>
+        )}
+    </div>
+);
+
+export default CheckBox;
