6c28755cc621e3a38130c2d1a48d606780efd827
Adrian Bielawski <adrian@bielaw.ski>
2021-06-30 10:08:37 +0100

    ArgsList added to ColorPicker

--- src/colorPicker/colorPicker.tsx
+++ src/colorPicker/colorPicker.tsx
@@ -1,20 +1,39 @@
-import React, { useCallback, useState } from "react";
-import { useParameter } from '@storybook/api';
+import React, { useCallback, useEffect, useState } from "react";
+import { useParameter, useGlobals, useStorybookState } from '@storybook/api';
 import { css, jsx } from '@emotion/react';
+// Utils
 import { findDefaultPaletteIndex, transformPalette } from "../utils/utils";
-import Colors from './colors';
-import PalettesList from "./palettesList/palettesList";
+// Types
 import { ColorPalettes } from "./types";
+// Components
+import Colors from './colors';
+import ArgsList from "./argsList/argsList";
+import PalettesList from "./palettesList/palettesList";
 /** @jsx jsx */
 
 const ColorPicker = () => {
     const colorPalettes: ColorPalettes = useParameter('colorPalettes');
     const defaultColorPalette = useParameter('defaultColorPalette', undefined);
+    const applyColorTo: string[] = useParameter('applyColorTo');
+    const [globals, updateGlobals] = useGlobals();
+    const state = useStorybookState();
     const [current, setCurrent] = useState(findDefaultPaletteIndex(
         colorPalettes.palettes,
         defaultColorPalette || colorPalettes.default
     ));
-    
+
+    if (!colorPalettes.palettes.length) {
+        return null
+    }
+
+    useEffect(() => {
+        updateGlobals({ selectedArgs: [] })
+    }, [state.storyId])
+
+    const handleArgsChange = (newArgs: string[]) => {
+        updateGlobals({ selectedArgs: newArgs })
+    }
+
     const handlePaletteChange = useCallback(
         (newCurrent: number) => {
             setCurrent(newCurrent)
@@ -70,11 +89,24 @@ const ColorPicker = () => {
             >
                 Click on color to copy to clipboard
             </div>
-            <PalettesList
-                palettes={colorPalettes.palettes}
-                current={current}
-                onChange={handlePaletteChange}    
-            />
+            <div css={css`
+                    display: flex;
+                    gap: 2em;
+                `}
+            >
+                <PalettesList
+                    palettes={colorPalettes.palettes}
+                    current={current}
+                    onChange={handlePaletteChange}
+                />
+                {(applyColorTo && applyColorTo.length > 0) && (
+                    <ArgsList
+                        args={applyColorTo}
+                        selected={globals.selectedArgs || []}
+                        onChange={handleArgsChange}
+                    />
+                )}
+            </div>
             <div>
                 {getColors()}
             </div>
