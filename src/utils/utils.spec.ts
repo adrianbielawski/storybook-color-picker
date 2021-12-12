0a64ffb1888c755fc1492ca9d21c1d0b36206967
Adrian Bielawski <adrian@bielaw.ski>
2021-12-12 12:45:44 +0000

    Utils tests adjusted

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,78 +1,140 @@
-import { transformPalette, findDefaultPaletteIndex } from './utils';
+import { PaletteObj, StatePalettes } from 'src/colorPicker/types'
+import { transformPalette, findDefaultPaletteIndex } from './utils'
 
 describe('transformPalette', () => {
-    const inputAsArray = [{
-        label: 'light',
-        values: [{
-            label: '100',
-            value: '#fff',
-        }],
-    }];
+	const inputAsArray: PaletteObj = {
+		name: 'foo',
+		palette: [
+			{
+				label: 'light',
+				values: [
+					{
+						label: '100',
+						value: '#fff',
+					},
+				],
+			},
+		],
+	}
 
-    const inputAsObject = {
-        light: {
-            100: '#fff',
-        },
-    };
+	const inputAsObject: PaletteObj = {
+		name: 'foo',
+		palette: {
+			light: {
+				100: '#fff',
+			},
+		},
+	}
 
-    const inputAsSimpleObject = {
-        white: '#fff',
-    };
+	const inputAsSimpleObject: PaletteObj = {
+		name: 'foo',
+		palette: {
+			white: '#fff',
+		},
+	}
 
-    const inputAsMixedObject = {
-        ...inputAsSimpleObject,
-        ...inputAsObject,
-    };
-    
-    const expected = [
-        {
-            label: 'light',
-            values: [{
-                label: '100',
-                value: '#fff',
-            }],
-        },
-    ];
+	const inputAsMixedObject: PaletteObj = {
+		name: 'foo',
+		palette: {
+			...inputAsSimpleObject.palette,
+			...inputAsObject.palette,
+		},
+	}
 
-    const expectedFromSimpleObject = [{
-        label: 'white',
-        values: [{
-            label: 'white',
-            value: '#fff',
-        }],
-    }];
+	const expected = [
+		{
+			label: 'light',
+			values: [
+				{
+					label: '100',
+					value: '#fff',
+				},
+			],
+		},
+	]
 
-    it.each([
-        ['input as array', inputAsArray, expected],
-        ['input as object', inputAsObject, expected],
-        ['input as simple object', inputAsSimpleObject, expectedFromSimpleObject],
-        ['input as mixed object', inputAsMixedObject, [...expectedFromSimpleObject, ...expected]],
-    ])('returns transformed palette correctly when %s', (desc, input, expected) => {
-        const output = transformPalette(input);
+	const expectedFromSimpleObject = [
+		{
+			label: 'white',
+			values: [
+				{
+					label: 'white',
+					value: '#fff',
+				},
+			],
+		},
+	]
 
-        expect(output).toEqual(expected);
-    })
-});
+	beforeEach(() => {
+		global.CSS = {
+			supports: jest.fn(),
+			escape: jest.fn()
+		}
+	})
+
+	it.each([
+		['input as array', inputAsArray, expected],
+		['input as object', inputAsObject, expected],
+		['input as simple object', inputAsSimpleObject, expectedFromSimpleObject],
+		['input as mixed object', inputAsMixedObject, [...expectedFromSimpleObject, ...expected]],
+	])('returns transformed palette correctly when %s', (desc, input, expectedPalette) => {
+		(global.CSS.supports as jest.Mock).mockReturnValue(true)
+
+		const expected = {
+			name: 'foo',
+			palette: expectedPalette,
+		}
+
+		const output = transformPalette(input)
+
+		expect(output).toEqual(expected)
+	})
+})
 
 describe('findDefaultPaletteIndex', () => {
-    const palettes = [
-      {
-        name: 'palette name',
-        palette: { '100': '#fff' },
-      },
-      {
-        name: 'palette',
-        palette: { '100': '#000' },
-      },
-    ]
+	const palettes = [
+		{
+			name: 'palette name',
+			palette: [
+				{
+					label: 'light',
+					values: [
+						{
+							label: '100',
+							value: '#fff',
+						},
+					],
+				},
+			],
+		},
+		{
+			name: 'palette',
+			palette: [
+				{
+					label: 'dark',
+					values: [
+						{
+							label: '900',
+							value: '#000',
+						},
+					],
+				},
+			],
+		},
+	]
 
-    it.each([
-     ['name is on the list', 'palette name', 0],  
-     ['name is on the list', 'palette', 1],  
-     ['name is NOT on the list', 'pal', 0],  
-    ])('return correct index when %s', (desc, defaultPalette, expected) => {
-        const output = findDefaultPaletteIndex(palettes, defaultPalette)
-        
-        expect(output).toEqual(expected);
-    })
+	it.each([
+		['name is on the list', 'palette name', 0],
+		['name is on the list', 'palette', 1],
+		['name is NOT on the list', 'foo', -1],
+	])('return correct index when %s', (desc, defaultPalette, expected) => {
+		const input = {
+			default: defaultPalette,
+			palettes,
+		}
+
+		const output = findDefaultPaletteIndex(input)
+
+		expect(output).toEqual(expected)
+	})
 })
