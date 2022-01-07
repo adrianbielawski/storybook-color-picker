d37f5c4e9ca87093530b174df8a043cfdcb09de1
Adrian Bielawski <adrian@bielaw.ski>
2022-01-07 15:34:36 +0000

    Deprecation warning function added

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -3,3 +3,4 @@ export { default as findPrimaryPaletteIndex } from "./findPrimaryPaletteIndex"
 export { default as getPrimaryPaletteName } from './getPrimaryPaletteName'
 export { default as transformPalette } from "./transformPalette"
 export { default as getColorPalettes } from './getColorPalettes'
+export { default as warnDeprecated } from './deprecation'
