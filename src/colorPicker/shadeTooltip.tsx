e32b399dfc828a7f64cac8c80a545cd5d73f3387
Adrian Bielawski <adrian@bielaw.ski>
2022-01-16 12:28:01 +0000

    Use text color from shade in tooltip

--- src/colorPicker/shadeTooltip.tsx
+++ src/colorPicker/shadeTooltip.tsx
@@ -9,34 +9,33 @@ type Props = {
 }
 
 const ShadeTooltip = (props: Props) => {
-    const text = props.shade.label ?
-        `${props.shade.label}: ${props.shade.value}`
-        : props.shade.value;
+	const color = props.shade.value
+	const label = props.shade.label
+	const textColor = props.shade.textColor
 
-    return (
-        <div
-            css={css`
-            padding: .2em .5em;
-            border-radius: .3em;
-            background-color: ${props.shade.value};
-        `}
-        >
-            <p
-                css={css`
-                white-space: nowrap;
-                font-size: 1.2em;
-                line-height: 0;
-                color: ${props.shade.value};
-                filter: saturate(0) grayscale(1) brightness(1) contrast(100000%) invert(1);
-            `}
-            >
-                {props.copied
-                    ? 'Copied'
-                    : text
-                }
-            </p>
-        </div>
-    )
-};
+	const text = label ?
+		`${label}: ${color}`
+		: color
 
-export default ShadeTooltip;
+	return (
+		<div css={css`
+			padding: .2em .5em;
+			border-radius: .3em;
+			background-color: ${color};
+		`}>
+			<p css={css`
+				white-space: nowrap;
+				font-size: 1.2em;
+				line-height: 0;
+				color: ${textColor};
+			`}>
+				{props.copied
+					? 'Copied'
+					: text
+				}
+			</p>
+		</div>
+	)
+}
+
+export default ShadeTooltip
