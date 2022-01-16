33e1c08f8662729e7af21e0c40f8c4d21d99d641
Adrian Bielawski <adrian@bielaw.ski>
2022-01-16 12:11:14 +0000

    textColor added to default palettes

--- src/constants/defaultPalettes/basic.ts
+++ src/constants/defaultPalettes/basic.ts
@@ -7,26 +7,32 @@ export default {
 				{
 					label: 'red',
 					value: '#f00',
+					textColor: '#fff',
 				},
 				{
 					label: 'yellow',
 					value: '#ff0',
+					textColor: '#000',
 				},
 				{
 					label: 'green',
 					value: '#0f0',
+					textColor: '#000',
 				},
 				{
 					label: 'light-blue',
 					value: '#0ff',
+					textColor: '#000',
 				},
 				{
 					label: 'blue',
 					value: '#00f',
+					textColor: '#fff',
 				},
 				{
 					label: 'purple',
 					value: '#f0f',
+					textColor: '#fff',
 				},
 			],
 		},
@@ -36,42 +42,52 @@ export default {
 				{
 					label: '50',
 					value: '#fff',
+					textColor: '#000',
 				},
 				{
 					label: '100',
 					value: '#f5f5f5',
+					textColor: '#000',
 				},
 				{
 					label: '200',
 					value: '#eee',
+					textColor: '#000',
 				},
 				{
 					label: '300',
 					value: '#e5e5e5',
+					textColor: '#000',
 				},
 				{
 					label: '400',
 					value: '#ddd',
+					textColor: '#000',
 				},
 				{
 					label: '500',
 					value: '#d5d5d5',
+					textColor: '#000',
 				},
 				{
 					label: '600',
 					value: '#ccc',
+					textColor: '#000',
 				},
 				{
 					label: '700',
 					value: '#c5c5c5',
+					textColor: '#000',
 				},
 				{
 					label: '800',
 					value: '#bbb',
+					textColor: '#000',
 				},
 				{
 					label: '900',
 					value: '#b5b5b5',
+					textColor: '#fff',
 				},
 			],
 		},
@@ -81,42 +97,52 @@ export default {
 				{
 					label: '50',
 					value: '#999',
+					textColor: '#fff',
 				},
 				{
 					label: '100',
 					value: '#888',
+					textColor: '#fff',
 				},
 				{
 					label: '200',
 					value: '#777',
+					textColor: '#fff',
 				},
 				{
 					label: '300',
 					value: '#666',
+					textColor: '#fff',
 				},
 				{
 					label: '400',
 					value: '#555',
+					textColor: '#fff',
 				},
 				{
 					label: '500',
 					value: '#444',
+					textColor: '#fff',
 				},
 				{
 					label: '600',
 					value: '#333',
+					textColor: '#fff',
 				},
 				{
 					label: '700',
 					value: '#222',
+					textColor: '#fff',
 				},
 				{
 					label: '800',
 					value: '#111',
+					textColor: '#fff',
 				},
 				{
 					label: '900',
 					value: '#000',
+					textColor: '#fff',
 				},
 			],
 		},
--- src/constants/defaultPalettes/colorful.ts
+++ src/constants/defaultPalettes/colorful.ts
@@ -7,66 +7,82 @@ export default {
 				{
 					label: '1',
 					value: '#f69679',
+					textColor: '#fff',
 				},
 				{
 					label: '2',
 					value: '#f9ad81',
+					textColor: '#000',
 				},
 				{
 					label: '3',
 					value: '#fdc689',
+					textColor: '#000',
 				},
 				{
 					label: '4',
 					value: '#fff799',
+					textColor: '#000',
 				},
 				{
 					label: '5',
 					value: '#c4df9b',
+					textColor: '#000',
 				},
 				{
 					label: '6',
 					value: '#a3d39c',
+					textColor: '#000',
 				},
 				{
 					label: '7',
 					value: '#82ca9c',
+					textColor: '#fff',
 				},
 				{
 					label: '8',
 					value: '#7accc8',
+					textColor: '#fff',
 				},
 				{
 					label: '9',
 					value: '#6dcff6',
+					textColor: '#fff',
 				},
 				{
 					label: '10',
 					value: '#7da7d9',
+					textColor: '#fff',
 				},
 				{
 					label: '11',
 					value: '#8393ca',
+					textColor: '#fff',
 				},
 				{
 					label: '12',
 					value: '#8781bd',
+					textColor: '#fff',
 				},
 				{
 					label: '13',
 					value: '#a186be',
+					textColor: '#fff',
 				},
 				{
 					label: '14',
 					value: '#bd8cbf',
+					textColor: '#fff',
 				},
 				{
 					label: '15',
 					value: '#f49ac1',
+					textColor: '#fff',
 				},
 				{
 					label: '16',
 					value: '#f5989d',
+					textColor: '#fff',
 				},
 			],
 		},
@@ -76,66 +92,82 @@ export default {
 				{
 					label: '1',
 					value: '#f26c4f',
+					textColor: '#fff',
 				},
 				{
 					label: '2',
 					value: '#f68e56',
+					textColor: '#fff',
 				},
 				{
 					label: '3',
 					value: '#fbaf5d',
+					textColor: '#000',
 				},
 				{
 					label: '4',
 					value: '#fff568',
+					textColor: '#000',
 				},
 				{
 					label: '5',
 					value: '#acd373',
+					textColor: '#000',
 				},
 				{
 					label: '6',
 					value: '#7cc576',
+					textColor: '#fff',
 				},
 				{
 					label: '7',
 					value: '#3cb878',
+					textColor: '#fff',
 				},
 				{
 					label: '8',
 					value: '#1cbbb4',
+					textColor: '#fff',
 				},
 				{
 					label: '9',
 					value: '#00bff3',
+					textColor: '#fff',
 				},
 				{
 					label: '10',
 					value: '#448ccb',
+					textColor: '#fff',
 				},
 				{
 					label: '11',
 					value: '#5674b9',
+					textColor: '#fff',
 				},
 				{
 					label: '12',
 					value: '#605ca8',
+					textColor: '#fff',
 				},
 				{
 					label: '13',
 					value: '#8560a8',
+					textColor: '#fff',
 				},
 				{
 					label: '14',
 					value: '#a864a8',
+					textColor: '#fff',
 				},
 				{
 					label: '15',
 					value: '#f06eaa',
+					textColor: '#fff',
 				},
 				{
 					label: '16',
 					value: '#f26d7d',
+					textColor: '#fff',
 				},
 			],
 		},
@@ -145,66 +177,82 @@ export default {
 				{
 					label: '1',
 					value: '#ed1c24',
+					textColor: '#fff',
 				},
 				{
 					label: '2',
 					value: '#f26522',
+					textColor: '#fff',
 				},
 				{
 					label: '3',
 					value: '#f7941d',
+					textColor: '#fff',
 				},
 				{
 					label: '4',
 					value: '#fff200',
+					textColor: '#000',
 				},
 				{
 					label: '5',
 					value: '#8dc63f',
+					textColor: '#fff',
 				},
 				{
 					label: '6',
 					value: '#39b54a',
+					textColor: '#fff',
 				},
 				{
 					label: '7',
 					value: '#00a651',
+					textColor: '#fff',
 				},
 				{
 					label: '8',
 					value: '#00a99d',
+					textColor: '#fff',
 				},
 				{
 					label: '9',
 					value: '#00aeef',
+					textColor: '#fff',
 				},
 				{
 					label: '10',
 					value: '#0072bc',
+					textColor: '#fff',
 				},
 				{
 					label: '11',
 					value: '#0054a6',
+					textColor: '#fff',
 				},
 				{
 					label: '12',
 					value: '#2e3192',
+					textColor: '#fff',
 				},
 				{
 					label: '13',
 					value: '#662d91',
+					textColor: '#fff',
 				},
 				{
 					label: '14',
 					value: '#92278f',
+					textColor: '#fff',
 				},
 				{
 					label: '15',
 					value: '#ec008c',
+					textColor: '#fff',
 				},
 				{
 					label: '16',
 					value: '#ed145b',
+					textColor: '#fff',
 				},
 			],
 		},
@@ -214,66 +262,82 @@ export default {
 				{
 					label: '1',
 					value: '#9e0b0f',
+					textColor: '#fff',
 				},
 				{
 					label: '2',
 					value: '#a0410d',
+					textColor: '#fff',
 				},
 				{
 					label: '3',
 					value: '#a36209',
+					textColor: '#fff',
 				},
 				{
 					label: '4',
 					value: '#aba000',
+					textColor: '#fff',
 				},
 				{
 					label: '5',
 					value: '#598527',
+					textColor: '#fff',
 				},
 				{
 					label: '6',
 					value: '#197b30',
+					textColor: '#fff',
 				},
 				{
 					label: '7',
 					value: '#007236',
+					textColor: '#fff',
 				},
 				{
 					label: '8',
 					value: '#00746b',
+					textColor: '#fff',
 				},
 				{
 					label: '9',
 					value: '#0076a3',
+					textColor: '#fff',
 				},
 				{
 					label: '10',
 					value: '#004a80',
+					textColor: '#fff',
 				},
 				{
 					label: '11',
 					value: '#003471',
+					textColor: '#fff',
 				},
 				{
 					label: '12',
 					value: '#1b1464',
+					textColor: '#fff',
 				},
 				{
 					label: '13',
 					value: '#440e62',
+					textColor: '#fff',
 				},
 				{
 					label: '14',
 					value: '#630460',
+					textColor: '#fff',
 				},
 				{
 					label: '15',
 					value: '#9e005d',
+					textColor: '#fff',
 				},
 				{
 					label: '16',
 					value: '#9e0039',
+					textColor: '#fff',
 				},
 			],
 		},
@@ -283,66 +347,82 @@ export default {
 				{
 					label: '1',
 					value: '#790000',
+					textColor: '#fff',
 				},
 				{
 					label: '2',
 					value: '#7b2e00',
+					textColor: '#fff',
 				},
 				{
 					label: '3',
 					value: '#7d4900',
+					textColor: '#fff',
 				},
 				{
 					label: '4',
 					value: '#827b00',
+					textColor: '#fff',
 				},
 				{
 					label: '5',
 					value: '#406618',
+					textColor: '#fff',
 				},
 				{
 					label: '6',
 					value: '#005e20',
+					textColor: '#fff',
 				},
 				{
 					label: '7',
 					value: '#005826',
+					textColor: '#fff',
 				},
 				{
 					label: '8',
 					value: '#005952',
+					textColor: '#fff',
 				},
 				{
 					label: '9',
 					value: '#005b7f',
+					textColor: '#fff',
 				},
 				{
 					label: '10',
 					value: '#003663',
+					textColor: '#fff',
 				},
 				{
 					label: '11',
 					value: '#002157',
+					textColor: '#fff',
 				},
 				{
 					label: '12',
 					value: '#0d004c',
+					textColor: '#fff',
 				},
 				{
 					label: '13',
 					value: '#32004b',
+					textColor: '#fff',
 				},
 				{
 					label: '14',
 					value: '#4b0049',
+					textColor: '#fff',
 				},
 				{
 					label: '15',
 					value: '#7b0046',
+					textColor: '#fff',
 				},
 				{
 					label: '16',
 					value: '#7a0026',
+					textColor: '#fff',
 				},
 			],
 		},
