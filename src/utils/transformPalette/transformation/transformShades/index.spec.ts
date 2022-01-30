e31223b96f14a1cff8d27dea1b866a240c6c480d
Adrian Bielawski <adrian@bielaw.ski>
2022-01-30 13:49:10 +0000

    transformShades tests

---
+++ src/utils/transformPalette/transformation/transformShades/index.spec.ts
@@ -0,0 +1,16 @@
+import { lightArray, transformedLightArray } from "src/utils/testsUtils"
+import transformShades from "."
+import getTextColor from "../getTextColor"
+
+jest.mock('../getTextColor')
+
+describe('transformShades', () => {
+  const getTextColorMock = jest.mocked(getTextColor)
+  it('transformes shades correctly', () => {
+   getTextColorMock.mockReturnValue('#000000')
+
+    const output = transformShades(lightArray.values)
+
+    expect(output).toEqual(transformedLightArray.values)
+  })
+})
